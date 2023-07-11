5b6f4731041b94706cb93055067cf9bbde4a64c3
Dharmik Kevadiya <dharmikkevadiya1552@gmail.com>
2023-07-11 18:41:48 +0530

    Resolved cors erorr

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: true,
-    credentials: true,
+    origin: "*", // Replace with the actual client URL
   })
 );
 app.use(compression());
